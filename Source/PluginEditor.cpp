72e6ea4f2db9655a88cbcc1512d35c2568534824
guilebarros <barros.guilherme@gmail.com>
2022-06-21 17:49:27 -0300

    Refactored Peak Coefficient Generator

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -134,6 +134,21 @@ void SimpleEQMacAudioProcessorEditor::resized()
     
 }
 
+void SimpleEQMacAudioProcessorEditor::parameterValueChanged(int parameterIndex, float newValue)
+{
+    parametersChanged.set(true);
+}
+
+void SimpleEQMacAudioProcessorEditor::timerCallback()
+{
+    if( parametersChanged.compareAndSetBool(false, true))
+    {
+        //update the monochain
+        
+        //signal a repaint
+    }
+}
+
 std::vector<juce::Component*> SimpleEQMacAudioProcessorEditor::getComps()
 {
     return
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,7 +23,9 @@ struct CustomRotarySlider : juce::Slider
 //==============================================================================
 /**
 */
-class SimpleEQMacAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQMacAudioProcessorEditor  : public juce::AudioProcessorEditor,
+juce::AudioProcessorParameter::Listener,
+juce::Timer
 {
 public:
     SimpleEQMacAudioProcessorEditor (SimpleEQMacAudioProcessor&);
@@ -32,11 +34,19 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    void parameterValueChanged (int parameterIndex, float newValue) override;
+    
+    
+    void parameterGestureChanged (int parameterIndex, bool gestureStarting) override {};
+    void timerCallback() override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQMacAudioProcessor& audioProcessor;
+    
+    juce::Atomic<bool> parametersChanged { false };
     
     CustomRotarySlider peakFreqSlider,
     peakGainSlider,
