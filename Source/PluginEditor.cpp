dfe11fdbff460d571db15e2fcfe0052cb7bcd7be
guilebarros <barros.guilherme@gmail.com>
2022-06-21 18:49:26 -0300

    Response Curve now Shows Cut Filter Response

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -161,6 +161,11 @@ void SimpleEQMacAudioProcessorEditor::timerCallback()
         auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
         updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         //signal a repaint
+        auto lowCutCoefficients = makeLowCutFilter(chainSettings, audioProcessor.getSampleRate());
+        auto highCutCoefficients = makeHighCutFilter(chainSettings, audioProcessor.getSampleRate());
+        
+        updateCutFilter(monoChain.get<ChainPositions::LowCut>(), lowCutCoefficients, chainSettings.lowCutSlope);
+        updateCutFilter(monoChain.get<ChainPositions::HighCut>(), highCutCoefficients, chainSettings.highCutSlope);
         repaint();
     }
 }
