4321ad42258bd7ca35438e72ac80c86afb879bc3
guilebarros <barros.guilherme@gmail.com>
2022-06-21 18:40:05 -0300

    Refactored How Cut Filters are created

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,13 +30,26 @@ highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlope
     {
         addAndMakeVisible(comp);
     }
-
-
+    
+    const auto& params = audioProcessor.getParameters();
+    
+    for(auto param : params)
+    {
+        param->addListener(this);
+    }
+    
+    startTimerHz(60);
     setSize (600,400);
 }
 
 SimpleEQMacAudioProcessorEditor::~SimpleEQMacAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    
+    for(auto param : params)
+    {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -144,8 +157,11 @@ void SimpleEQMacAudioProcessorEditor::timerCallback()
     if( parametersChanged.compareAndSetBool(false, true))
     {
         //update the monochain
-        
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         //signal a repaint
+        repaint();
     }
 }
 
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -255,10 +255,9 @@ void updateCoefficients(Coefficients& old, const Coefficients& replacements)
 void SimpleEQMacAudioProcessor::updateLowCutFilters(const ChainSettings &chainSettings)
 
 {
-    auto cutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq, getSampleRate(),  2 * (chainSettings.lowCutSlope + 1));
+    auto cutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
     
     auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
-    updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
     auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
     
     updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
@@ -268,7 +267,7 @@ void SimpleEQMacAudioProcessor::updateLowCutFilters(const ChainSettings &chainSe
 
 void SimpleEQMacAudioProcessor::updateHighCutFilters(const ChainSettings &chainSettings)
 {
-    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq, getSampleRate(), 2 * (chainSettings.highCutSlope + 1));
+    auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
     
     auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
     auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
