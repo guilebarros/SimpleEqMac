b28d6d9342b5e0f37f2a917e33c74d74a10f9623
guilebarros <barros.guilherme@gmail.com>
2022-06-22 23:37:20 -0300

    Labels Position Pre Setup

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -78,8 +78,6 @@ void RotarySliderWithLabels::paint(juce::Graphics &g)
     
     auto sliderBounds = getSliderBounds();
     
-// retângulos/conteiners dos sliders
-    
 //    g.setColour(Colours::red);
 //    g.drawRect(getLocalBounds());
 //    g.setColour(Colours::yellow);
@@ -91,6 +89,30 @@ void RotarySliderWithLabels::paint(juce::Graphics &g)
                                       sliderBounds.getHeight(),
                                       jmap(getValue(),range.getStart(), range.getEnd(), 0.0, 1.0), startAng,
                                       endAng, *this);
+    
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius = sliderBounds.getWidth() * 0.5f;
+    
+    g.setColour(Colour(0u, 172u, 1u));
+    g.setFont(getTextHeight());
+    
+    auto numChoices = labels.size();
+    for( int i = 0; i < numChoices; i++)
+    {
+        auto pos = labels[i].pos;
+        jassert(0.f <= pos);
+        jassert(pos <= 1.f);
+        
+        auto ang = jmap(pos,0.f, 1.f, startAng, endAng);
+        
+        auto c = center.getPointOnCircumference(radius + getTextHeight() * 0.5f + 1, ang);
+        Rectangle<float> r;
+        auto str = labels[i].label;
+        r.setSize(g.getCurrentFont().getStringWidth(str), getTextHeight());
+        r.setCentre(c);
+        r.setY(r.getY() + getTextHeight());
+        g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+    }
     
 }
 
